4dde2a1515ee1de83683988798778136f2a4e16f
Jainam Shah <61734413+jainam2385@users.noreply.github.com>
2021-12-19 21:01:16 +0530

    Sign up login

--- Searchingzilla/accounts/urls.py
+++ Searchingzilla/accounts/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from accounts.views import *
 
 urlpatterns = [
-    path('', include("accounts.urls")),
-    path('admin/', admin.site.urls),
+    path("", home, name="home"),
+    path("loginSignup", loginSignup, name="loginSignup")
 ]
--- Searchingzilla/accounts/views.py
+++ Searchingzilla/accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import redirect
+from django.urls import reverse
 
 # Create your views here.
+
+
+def home(request):
+    if request.method == "GET":
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(reverse("loginSignup"))
+        else:
+            return HttpResponseRedirect(reverse("loginSignup"))
+
+
+def loginSignup(request):
+    if request.method == "POST":
+        print(request.POST)
+
+    return render(request,  "LoginSignup.html")
